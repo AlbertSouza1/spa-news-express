40dc07d2363a61a3e86d06dd8aa0ba9b06a85e2d
Albert Souza <albert.neves.souza@gmail.com>
2026-01-07 22:06:59 -0300

    improve: add card styled

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
 import { Card } from "../../components/Cards/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { news } from '../../Data.js';
+import { HomeBody } from "./HomeStyled.jsx";
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            {news.map(item =>
-                <Card key={item.id} {...item} />
-            )}
+            <HomeBody>
+                {news.map(item =>
+                    <Card key={item.id} {...item} />
+                )}
+            </HomeBody>
         </>
     )
 }
